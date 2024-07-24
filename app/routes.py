05f3ae60c9762cbb462a8730d9f22dce846764f4
erickkendall <ekendallgithub@gmail.com>
2024-07-24 16:30:15 -0500

    Chapter 11: Facelift (v0.11)

--- app/routes.py
+++ app/routes.py
@@ -79,7 +79,7 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
+    user = User.query.filter_by(username=username).one_or_404()
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
